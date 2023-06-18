f3e8686c6ab51ec2f339baa9cf587c08026e6c13
Aditya828-ctrl <aditya.choraria@outlook.com>
2023-06-18 19:06:55 +0530

    lecture 61

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-const http= require('http');
-
 const express=require('express');
 
 const app=express();
 
 app.use((req,res,next) => {
     console.log("in the middleware");
-    next();// this makes the rerquest to continue to next middleware in line
+    next();// this makes the request to continue to next middleware in line
 });
 
 app.use((req,res,next) => {
     console.log("in another middleware");
+    res.send('<h1>Hello form express</h1>');
 });
 
-const server= http.createServer(app);
+// const server= http.createServer(app);
 
-server.listen(3000); //3000 is port no.
+// server.listen(3000); //3000 is port no.
+
+app.listen(3000);
